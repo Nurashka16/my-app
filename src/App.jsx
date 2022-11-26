f03b321e4fa2998e2149b682c03106b7d6796148
Nuraiym Tilepova <bangwow1602@gmail.com>
2022-11-26 12:53:09 +0300

    add base route

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,19 @@ import Navbar from "./Components/Navbar/Navbar";
 import Profile from "./Components/Profile/Profile";
 import Dialogs from "./Components/Dialogs/Dialogs";
 import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./Components/Home/Home";
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="app-wrapper">
-        <Header />
-        <Navbar />
-        <div className="app-wrapper-content">
-          <Routes>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/dialogs" element={<Dialogs />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Header />}>
+            <Route index element={<Home />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="dialogs" element={<Dialogs />} />
+          </Route>
+        </Routes>
       </div>
     </BrowserRouter>
   );
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom";
 import style from "./Header.module.css";
 const Header = () => {
-   return <header className={style.header}>
-   <img src="https://www.logodesignlove.com/wp-content/uploads/2022/01/logo-wave-symbol-01.jpg"></img>
- </header>
-}
+  return (
+    <div>
+      <header className={style.header}>
+        <img src="https://www.logodesignlove.com/wp-content/uploads/2022/01/logo-wave-symbol-01.jpg"></img>
+      </header>
+      <Outlet />
+      -------------------------------- Контент дальше-------------------------------------------
+    </div>
+  );
+};
 export default Header;
