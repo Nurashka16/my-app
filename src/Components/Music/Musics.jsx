1f67c9446e7835153606df034d2e7d8b6ebbc40c
Nuraiym Tilepova <bangwow1602@gmail.com>
2023-06-30 16:40:08 +0300

    music доработан

--- src/Components/Music/Musics.jsx
+++ src/Components/Music/Musics.jsx
@@ -7,41 +7,15 @@ export const Musics = (props) => {
   let actual = props.musicPage.musicActual;
   let musics = props.musicPage.musics.map((m) => (
     <div>
-      <Sing /*onclick={
-        (e)=> {
-        props.playAC(m);
-      }}*/
-        id={m.id}
-        key={m.id}
-        author={m.author}
-        musicLength={m.musicLength}
-        play={m.play}
-        url={m.url}
-        //playAc={props.playAc}
+      <Sing data={m}
+        playAC={props.playAC}
       />
     </div>
   ));
   return (
     <div>
-      <div className={style.act}>{actual ? "no null" : musics[0] }</div>
+      <div className={style.act}>{actual ? <Sing data={actual.data}/> : musics[0] }</div>
       <div>{musics}</div>
     </div>
   );
 };
-/*   id: 3,
-      url: "https://btsarmy.ru/_pu/0/43245221.jpg",
-      musicName: "KroCi",
-      author: "MsNash",
-      musicLength: "4:01",
-      play: false,
-    <SingContainer
-      id={m.id} key={m.id}
-    />
-  ));
-  return (
-    <div>
-      <div>
-        {actual == null ? null : <Actual/> }
-      </div>
-      <div>{musics}</div>
-    </div>*/
--- src/Components/Music/Sing/Sing.jsx
+++ src/Components/Music/Sing/Sing.jsx
@@ -3,16 +3,18 @@ import style from "./Sing.module.css";
 
 export const Sing = (props) => {
   return (
-    <div className={style.block}>
+    <div className={style.block} onClick={(e)=> {
+      props.playAC(props);
+    }}>
       <div>
-        <img className={style.img} src={props.url}/>
+        <img className={style.img} src={props.data.url}/>
       </div>
       <div className={style.title}>
         <div className={style.name}>
-          {props.name}</div>
-        <div className={style.author}>{props.author}</div>
+          {props.data.name}</div>
+        <div className={style.author}>{props.data.author}</div>
       </div>
-      <div className={style.length}>{props.length}</div>
+      <div className={style.length}>{props.data.length}</div>
     </div>
   );
 };
